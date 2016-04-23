2116e9cf34a1b529ad4ba601b5c520739a7b687a
Maksym Kyrpychnyk <mkirpichnik@gmail.com>
2016-04-23 05:34:36 +0300

    added files for before post

--- js/authPage.js
+++ js/authPage.js
@@ -1,7 +1,15 @@
 ﻿$(function () {
-    $('.single-item').slick({
+     $('.single-item').slick({
         dots: true,
         infinite: false,
         speed: 600
-    });
+     });
+
+     $('.news-slider').slick({
+         dots: true,
+         infinite: false,
+         speed: 600,
+         slidesToShow: 3,
+         slidesToScroll: 3
+     });
 });
