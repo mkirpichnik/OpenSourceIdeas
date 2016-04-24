bdf2587f487f503665faa096179c84735e72c529
Maksym Kyrpychnyk <mkirpichnik@gmail.com>
2016-04-24 18:17:01 +0300

    added page (addIdeaPage)

--- js/authPage.js
+++ js/authPage.js
@@ -13,9 +13,9 @@
          slidesToScroll: 3
      });
 
-     //$('#test').tagEditor({
-     //    initialTags: ['Hello', 'World', 'Example', 'Tags'],
-     //    delimiter: ', ', /* space and comma */
-     //    placeholder: 'Enter tags ...'
-     //});
+    $('.idea-tags').tagEditor({
+         initialTags: ['Hello', 'World', 'Example', 'Tags'],
+         delimiter: ', ', /* space and comma */
+         placeholder: 'Enter tags ...'
+     });
 });
