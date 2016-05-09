7256b3c7f43a5c79947da75a45a2f3a8011df659
Maksym Kyrpychnyk <mkirpichnik@gmail.com>
2016-05-09 14:55:39 +0300

    added notificatioins, changed design of edit pages and preview idea page

---
+++ js/editPage.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+    $('.btn-tooltip').tooltip();
+    $('.btn-save').on('click', function () {
+        $('.notification-save').fadeIn();
+    });
+    $('.btn-cancel').on('click', function () {
+        $('.notification-cancel').fadeIn();
+    });
+});
